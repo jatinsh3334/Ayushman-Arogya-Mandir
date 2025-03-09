7fcdcbf3abf7e1a111d11a82d5c4b4feb153cf4c
jatinsh3334 <jatinsharma4350@gmail.com>
2025-03-09 20:17:10 +0530

    basic setup for Ayushman

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styling/Homepage.css'
+
+
+export default function Homepage() {
+  return (
+    <div>
+        <p>Ayushman Homepage!</p>
+      
+    </div>
+  )
+}
